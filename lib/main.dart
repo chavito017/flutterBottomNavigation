8a1a737641e5f32b758cdbaf82082432fa5e78e5
chavito017 <kd3223739115@.com>
2023-11-07 19:01:38 -0500

    creando una barra de navegacion en Fluter

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ejercicios/presentation/screens/main_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
